3543d97531df66f25f27f493f48adf1ffe6abc95
matthewhu8 <161522395+matthewhu8@users.noreply.github.com>
2024-08-20 15:20:31 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,9 +15,9 @@
 # ----------------------------------------
 
 PLUGIN_NAME="$(basename $(dirname $(realpath $0)))"  # name of current directory
-PLUGIN_TITLE='My ChRIS Plugin'
+PLUGIN_TITLE='querypacs'
 SCRIPT_NAME='commandname'
-DESCRIPTION='A ChRIS plugin to do something awesome'
+DESCRIPTION='populates the datetree with json files from a database based on study date'
 ORGANIZATION='FNNDSC'
 EMAIL='dev@babyMRI.org'
 
@@ -46,7 +46,7 @@ ENABLE_ACTIONS_BUILD=yes
 # STEP 2. Uncomment the line where it says READY=yes
 # ----------------------------------------
 
-#READY=yes
+READY=yes
 
 # ----------------------------------------
 # STEP 3. Run: ./bootstrap.sh
